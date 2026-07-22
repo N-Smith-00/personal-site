6bd9b366ebfa2c1c5df1b34aa9e2715d85ff3893
Nicholas-Smith463 <Nicholas.Smith463@gmail.com>
2026-07-22 18:15:45 -0400

    main page done, template wip

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Website
+
+Colour palette: <https://coolors.co/0e1113-232a2f-707070-3d2000-25650c-f4f4f5>
---
+++ scripts/projects-home.js
@@ -0,0 +1,58 @@
+const projects = [
+    {
+        id: "",
+        title: "This Webpage",
+        description: "My personal website to showcase my projects, education, and experience",
+        image: "",
+        technologies: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+        id: "through-the-labyrinth",
+        title: "Through the Labyrinth",
+        description: "A roguelike game focused on creating unique experiences through perspective based gameplay",
+        image: "",
+        technologies: ["Unity", "C#"],
+    },
+    {
+        id: "trial-of-the-warden",
+        title: "Trial of the Warden",
+        description: "A virtual reality game where the player must defeat something",
+        image: "images/trial-sc.png",
+        technologies: ["Unity", "C#", "Meta XR"],
+    },
+    {
+        id: "barbell-calc",
+        title: "Barbell Calculator",
+        description: "A tool for calculating plate configurations on barbells",
+        image: "",
+        technologies: ["HTML", "CSS", "Python", "Flask"]
+    }
+];
+
+const projectsGrid = document.getElementById("projects-grid");
+
+projects.forEach(project => {
+    const card = document.createElement("a");
+    card.classList.add("project");
+    if (project.id) {
+        card.href = `projects/${project.id}.html`;
+    } else {
+        card.href = "#";
+    }
+
+    card.innerHTML = `
+        <div class="card-container">
+            <h2 class="project-title">${project.title}</h2>
+            <img src=${project.image} alt="">
+            <p>${project.description}</p>
+            <div class="project-tech">
+                ${project.technologies.map(technology => `
+                    <span class="tech-tag">
+                        ${technology}
+                    </span>
+                `).join("")}
+            </div>
+        </div>
+    `
+    projectsGrid.appendChild(card)
+});
